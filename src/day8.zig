54230a3f6df96809eea6e4cf5b289e598eb807eb
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2023-12-09 00:59:26 -0500

    Wowee day 8 done

--- src/day8.zig
+++ src/day8.zig
@@ -7,16 +7,9 @@ const Pair = struct {
     right: Location,
 };
 
-const LocationContext = struct {
-    const Self = @This();
-    fn hash(_: *Self, loc: Location) u64 {
-        return loc[0] << 16 + loc[1] << 8 + loc[2];
-    }
-
-    fn eql(_: *Self, a: Location, b: Location) bool {
-        return a[0] == b[0] and a[1] == b[1] and a[2] == b[2];
-    }
-};
+fn loc_eql(a: Location, b: Location) bool {
+    return a[0] == b[0] and a[1] == b[1] and a[2] == b[2];
+}
 
 pub fn part1(file: *std.fs.File) !i32 {
     var buf: [20]u8 = undefined;
@@ -38,8 +31,7 @@ pub fn part1(file: *std.fs.File) !i32 {
 
     var curr_location: [3]u8 = .{ 'A', 'A', 'A' };
     var steps: usize = 0;
-    var loc_cxt = LocationContext{};
-    while (!loc_cxt.eql(curr_location, .{ 'Z', 'Z', 'Z' })) {
+    while (!loc_eql(curr_location, .{ 'Z', 'Z', 'Z' })) {
         const dir = directions[steps % directions.len];
         steps += 1;
         const this_loc = locations.get(curr_location).?;
@@ -53,9 +45,9 @@ pub fn part1(file: *std.fs.File) !i32 {
     return @intCast(steps);
 }
 
-const Path = struct {
-    zs: std.ArrayList(u16),
-    end: Location,
+const Cycle = struct {
+    zs: std.ArrayList(usize),
+    steps: usize,
 };
 
 pub fn part2(file: *std.fs.File) !i32 {
@@ -81,90 +73,102 @@ pub fn part2(file: *std.fs.File) !i32 {
         }
     }
 
+    var initial_steps: usize = 0;
+    var all_z = false;
     std.debug.print("{any}\n", .{current_locations.items});
-    var caches = std.AutoArrayHashMap(Location, Path).init(alloc.allocator());
-    defer caches.deinit();
-    var iter = locations.iterator();
-    while (iter.next()) |loc| {
-        var curr = loc.key_ptr.*;
-        var steps: u16 = 0;
-        var zs = std.ArrayList(u16).init(alloc.allocator());
-        for (directions) |dir| {
-            const this_loc = locations.get(curr).?;
+    while (!all_z) {
+        const dir = directions[initial_steps % directions.len];
+        initial_steps += 1;
+        all_z = true;
+        for (current_locations.items) |*loc| {
+            const this_loc = locations.get(loc.*).?;
             if (dir == 'L') {
-                curr = this_loc.left;
+                loc.* = this_loc.left;
             } else {
-                curr = this_loc.right;
+                loc.* = this_loc.right;
             }
-            if (curr[2] == 'Z') {
-                try zs.append(steps);
+            if (loc[2] != 'Z') {
+                all_z = false;
             }
-            steps += 1;
         }
-        try caches.put(loc.key_ptr.*, Path{ .zs = zs, .end = curr });
+        if (initial_steps == locations.count() * directions.len) {
+            break;
+        }
+    }
+    if (all_z) {
+        std.debug.print("{d}\n", .{initial_steps});
+        return 0;
     }
 
-    var cache_iter_2 = caches.iterator();
-    while (cache_iter_2.next()) |cache| {
-        std.debug.print("{any}, {any}/{any}\n", .{ cache.key_ptr.*, cache.value_ptr.*.end, cache.value_ptr.*.zs.items });
-    }
-    var steps: usize = 0;
+    std.debug.print("{d}\n", .{initial_steps});
     std.debug.print("{any}\n", .{current_locations.items});
-    const step_count: u16 = outer: while (true) : (steps += 1) {
-        const first = current_locations.items[0];
-        step_iter: for (caches.get(first).?.zs.items) |step| {
-            location: for (current_locations.items[1..]) |loc| {
-                if (caches.get(loc).?.zs.items.len == 0) {
-                    break :step_iter;
+    var loops = std.ArrayList(Cycle).init(alloc.allocator());
+    for (current_locations.items) |item| {
+        var zs = std.ArrayList(usize).init(alloc.allocator());
+        var current = item;
+        var steps: usize = 0;
+        var cycles: usize = 0;
+        while (!loc_eql(current, item) or cycles == 0) {
+            for (directions) |dir| {
+                const this_loc = locations.get(current).?;
+                if (dir == 'L') {
+                    current = this_loc.left;
+                } else {
+                    current = this_loc.right;
                 }
-                for (caches.get(loc).?.zs.items) |z| {
-                    if (z == step) {
-                        continue :location;
+                if (current[2] == 'Z') {
+                    try zs.append(steps);
+                }
+                steps += 1;
+            }
+            cycles += 1;
+        }
+        std.debug.print("{s} {d} {any}\n", .{ item, cycles, zs.items });
+        try loops.append(Cycle{ .zs = zs, .steps = steps });
+    }
+
+    var initial = loops.items[0];
+    var temp_zs = std.ArrayList(usize).init(alloc.allocator());
+    for (loops.items[1..]) |loop| {
+        const combined_cycle: usize = (initial.steps / std.math.gcd(initial.steps, loop.steps)) * loop.steps;
+        std.debug.print("{d} {any}\n", .{ combined_cycle, initial.zs.items });
+        for (initial.zs.items) |item| {
+            try temp_zs.append(item);
+        }
+        initial.zs.clearRetainingCapacity();
+        while (temp_zs.items[0] < combined_cycle and loop.zs.items[0] < combined_cycle) {
+            // std.debug.print("{any} {any}\n", .{ temp_zs.items, loop.zs.items });
+            if (temp_zs.items[0] > loop.zs.items[loop.zs.items.len - 1]) {
+                for (loop.zs.items) |*item| {
+                    item.* += loop.steps;
+                }
+            } else if (loop.zs.items[0] > temp_zs.items[temp_zs.items.len - 1]) {
+                for (temp_zs.items) |*item| {
+                    item.* += initial.steps;
+                }
+            } else {
+                for (temp_zs.items) |a| {
+                    for (loop.zs.items) |b| {
+                        if (a == b) {
+                            try initial.zs.append(a);
+                        }
                     }
                 }
-                continue :step_iter;
+                for (temp_zs.items) |*item| {
+                    item.* += initial.steps;
+                }
+                for (loop.zs.items) |*item| {
+                    item.* += loop.steps;
+                }
             }
-            break :outer step;
         }
-        for (current_locations.items) |*loc| {
-            loc.* = caches.get(loc.*).?.end;
-        }
-        if (steps % 10000000 == 0) {
-            std.debug.print("{d}\n", .{steps});
-        }
-    };
-
-    var cache_iter = caches.iterator();
-    while (cache_iter.next()) |cache| {
-        cache.value_ptr.zs.deinit();
+        loop.zs.deinit();
+        temp_zs.clearRetainingCapacity();
+        initial.steps = combined_cycle;
+        std.debug.print("{d} {any}\n", .{ combined_cycle, initial.zs.items });
     }
 
-    const step_count_u: usize = @intCast(step_count);
-    const total_step_count = steps * directions.len + step_count_u + 1;
-    std.debug.print("{d}*{d} + {d}\n", .{ steps, directions.len, step_count_u + 1 });
-    std.debug.print("{d}\n", .{total_step_count});
-    return 0;
+    std.debug.print("{d}\n", .{initial.zs.items[0] + initial_steps});
 
-    // var steps: usize = 0;
-    // var all_z = false;
-    // std.debug.print("{any}\n", .{current_locations.items});
-    // while (!all_z) {
-    //     const dir = directions[steps % directions.len];
-    //     steps += 1;
-    //     all_z = true;
-    //     for (current_locations.items) |*loc| {
-    //         const this_loc = locations.get(loc.*).?;
-    //         if (dir == 'L') {
-    //             loc.* = this_loc.left;
-    //         } else {
-    //             loc.* = this_loc.right;
-    //         }
-    //         if (loc[2] != 'Z') {
-    //             all_z = false;
-    //         }
-    //     }
-    //     if (steps % 10000000 == 0) {
-    //         std.debug.print("{d}\n", .{steps});
-    //     }
-    // }
+    return 0;
 }
